79e5cb8ffda297c3ca1bc2c16b5bcde0f275b94b
unknown <pabloduranhernandez2004@gmail.com>
2026-04-16 19:09:44 +0200

    Remove leftover build artifacts

--- electron/debug.js
+++
@@ -1,7 +0,0 @@
-const e = require('electron');
-console.log('type:', typeof e);
-if (typeof e === 'object' && e !== null) {
-  console.log('keys:', Object.keys(e).join(','));
-} else {
-  console.log('value:', String(e).slice(0, 100));
-}
